0dac64c6f55157afe37900ecceda058ee6659e5d
Yuri Oliveira <yuri12194@gmail.com>
2020-08-25 14:43:36 -0300

    s4-step3 (configuration of limits service).

--- src/main/java/com/microservices/limitsservice/LimitConfigurationController.java
+++ src/main/java/com/microservices/limitsservice/LimitConfigurationController.java
@@ -1,5 +1,6 @@
 package com.microservices.limitsservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +9,11 @@ import com.microservices.limitsservice.bean.LimitConfiguration;
 @RestController
 public class LimitConfigurationController {
 	
+	@Autowired
+	private Configuration configuration;
+	
 	@GetMapping("/limits")
 	public LimitConfiguration retrieveLimitsFromConfigurations() {
-		return new LimitConfiguration(1000, 1);
+		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
 	}
 }
